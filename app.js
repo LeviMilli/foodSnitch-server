db2f3d9bd2bdc67d3ae808eef6dcf3bde66e0f32
Levi Milli <107660704+LeviMilli@users.noreply.github.com>
2022-11-12 17:01:21 -0800

    server attempt

--- app.js
+++ app.js
@@ -10,17 +10,9 @@ require("./db");
 const express = require("express");
 const app = express();
 const cors = require('cors');
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.get('/', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all'})
-})
 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+app.use(cors())
+
 
 
 const session = require('express-session');
