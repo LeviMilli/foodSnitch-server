e52593c278f6dccea841902eef1daa8cdcf941b4
unknown <levi.millikin@gmail.com>
2022-12-19 23:04:21 -0800

    moved listen to connect for serverless

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5005;
+
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
@@ -10,6 +12,9 @@ const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/foodSnitch";
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on port http://localhost:${PORT}`);
+    });
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
